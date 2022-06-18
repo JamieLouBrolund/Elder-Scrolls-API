ada516e6466bd33f861c8985a5775438332e4f5e
Jamie Lou Brolund <94914187+JamieLouBrolund@users.noreply.github.com>
2022-06-18 16:41:30 -0500

    added console log to api

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(cors())
 
 
 const race = {
-    'Altmer': {
-        'raceName': 'Altmer',
+    'altmer': {
+        'raceName': 'altmer',
         'knownAs': 'High Elf',
         'homeland': 'Summerset Isles',
         'uniqueSkills': 'illusion, conjuration, destruction, restoration, alteration, enchanting',
@@ -19,7 +19,7 @@ const race = {
         'history': "The Altmer are one of the oldest races on Tamriel, one of the earliest descendants of the original mer race of Aldmer, and from there, through the Ehlnofey to the divine et'Ada themselves. As a race, they are extremely proud of their Aldmeri heritage, and make a concerted effort to maintain their genetic lineage. Their name translates from the Aldmeri language as High Ones or Cultured Ones. For their part, the Altmer argue that they are the first true culture on Tamriel, and still the most civilized race, an argument that even human scholars find difficult to refute.",
         'img': 'insert'
     },
-    'Argonian': {
+    'argonian': {
         'raceName': 'Argonian',
         'knownAs': 'Argonian',
         'homeland': 'Black Marsh',
@@ -31,7 +31,7 @@ const race = {
         'history': 'Enigmatic and intelligent, the Argonians are experts at guerrilla warfare, and their natural abilities suit their swampy homeland, Black Marsh. They have developed immunities to the diseases that have doomed many would-be explorers in the region, and they are capable of easily exploring underwater locations due to their ability to breathe underwater. Argonians make proficient thieves, due to their increased lockpicking and sneaking skills',
         'img': 'insert'
     },
-    'Bosmer': {
+    'bosmer': {
         'raceName': 'Bosmer',
         'knownAs': 'Wood Elf',
         'homeland': 'Valenwood',
@@ -43,7 +43,7 @@ const race = {
         'history': 'Bosmer (or, more commonly, Wood Elves) are the elven people of Valenwood. They prefer a simple existence, living in harmony with the land and wild animals. They are known to be the best archers in all of Tamriel and are known for their ability to command wild creatures. They make great scouts or thieves, due to their natural stealth and light footing.',
         'img': 'insert'
     },
-    'Breton': {
+    'breton': {
         'raceName': 'Breton',
         'knownAs': 'Breton',
         'homeland': 'High Rock',
@@ -55,7 +55,7 @@ const race = {
         'history': 'Bretons are a race of both human and elven ancestry.[1] They populate the province of High Rock. They are excellent mages[2] with high magic resistance, but have few other distinctive features. They are considered an intelligent race in Tamriel, known for a proficiency in abstract thinking,[source?] a possible reason for their adeptness in the magical arts. It is suggested that they may also carry Aldmer blood, which would further account for their magical prowess.',
         'img': 'insert'
     },
-    'Dunmer': {
+    'dunmer': {
         'raceName': 'Dunmer',
         'knownAs': 'Dark Elf',
         'homeland': 'Morrowind',
@@ -67,7 +67,7 @@ const race = {
         'history': "The Dunmer, more commonly known as Dark Elves, are the grey-skinned elven natives of the province of Morrowind. After the eruption of Red Mountain, many Dunmer fled to Skyrim, where they have endured extreme prejudice as a result of their elven heritage and status as refugees. In spite of this, many Dunmer have been able to establish themselves in Tamriel's northernmost province as skillful merchants and mages.",
         'img': 'insert'
     },
-    'Imperial': {
+    'imperial': {
         'raceName': 'Imperial',
         'knownAs': 'Imperial',
         'homeland': 'Cyrodiil',
@@ -79,7 +79,7 @@ const race = {
         'history': 'Imperials are some of the most well-educated, wealthy and well-spoken of the races in Tamriel and the natives of the cosmopolitan province of Cyrodiil. Imperials are also known for their discipline and training of their citizen armies. Because of this, the Imperials have dominated Tamriel for more than 2,000 years.',
         'img': 'insert'
     },
-    'Khajiit' : {
+    'khajiit' : {
         'raceName': 'Khajiit',
         'knownAs': 'Khajiit',
         'homeland': 'Elsweyr',
@@ -91,7 +91,7 @@ const race = {
         'history': 'The Khajiit are a playable race present in The Elder Scrolls V: Skyrim. Khajiit are one of the beast races which inhabit the continent of Tamriel, primarily their home province of Elsweyr. The Khajiit have feline appearance and sly accent common to Cathay, however their type is not specified. Along with the Argonians, they are referred to as the beast-races of Skyrim. Because of this, Khajiit have a negative public image. They are the rarest race encountered in Skyrim, most commonly found only in caravans.',
         'img': 'insert'
     },
-    'Nord' : {
+    'nord' : {
         'raceName': 'Nord',
         'knownAs': 'Nord',
         'homeland': 'Skyrim',
@@ -103,7 +103,7 @@ const race = {
         'history': "Nords are a race that were led to Skyrim by Ysgramor. They are tall (standing at a scale of 1.03), fair-haired and pale-skinned humans from Atmora who are known for their incredible resistance to cold and even magical frost. They are enthusiastic warriors, and act as soldiers, mercenaries, merchants and blacksmiths all over Tamriel. Eager to augment their martial skills beyond the traditional methods of Skyrim, they excel in all manner of warfare. Above all else in Nord culture is the quest for honor and glory, and a great emphasis is placed on family values. They thrive in the cold, reminiscent of their native Atmora, and are known as a militant people by their neighbors. Nords are also naturally superior at sea, and have benefited from nautical trade since their first migrations across the sea from Atmora. They captain and crew the merchant fleets of many regions, and may be found all along Tamriel's coasts. They issue a battle cry to make their enemies flee for a short time. Nords comprise the majority of the Stormcloaks, as well as the majority of the population of Skyrim.",
         'img': 'insert'
     },
-    'Orsimer' : {
+    'orsimer' : {
         'raceName': 'Orsimer',
         'knownAs': 'Orc',
         'homeland': 'High Rock and Orsinium',
@@ -115,7 +115,7 @@ const race = {
         'history': "The Orsimer (more commonly known as Orcs), are the native people of the Wrothgarian and Dragontail Mountains. They are possibly a variant of elves or mer. Other sources state they are beastfolk. Following in the footsteps of Trinimac, and subsequently Malacath, Orcs have consistently held a standard as a race as some of Tamriel's greatest warriors and smiths.",
         'img': 'insert'
     },
-    'Redguard' : {
+    'redguard' : {
         'raceName' : 'Redguard',
         'knownAs' : 'Redguard',
         'homeland' : 'Hammerfell',
@@ -134,12 +134,14 @@ app.get('/', (req,res) => {
 })
 
 app.get('/api/:raceName', (req,res) => {
+    console.log(race)
     const racesName = req.params.raceName.toLowerCase()
     if(race[racesName]){
         res.json(race[racesName])
     }else{
         res.json(race['not a playable race'])
     }
+    
 })
 
 app.listen(process.env.PORT || PORT, () => { console.log(`server active on PORT ${PORT}`)
